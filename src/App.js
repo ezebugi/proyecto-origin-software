18865715c91fd91ffbaf12c4936fe6b5c0dd805e
ezequiel bugallo <ezequielbugallo@gmail.com>
2023-04-21 16:41:29 -0300

    corrigiendo las rutas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import LoginForm from "./components/Login/LoginForm";
+import { Navbar } from "./components/Navbar/Navbar";
+import React, { useState } from "react";
 import ItemListContainer from "./components/ItemList/ItemListContainer";
 import { ItemDetailContainer } from "./components/ItemDetail/ItemDetailContainer";
-import LoginForm from "./components/Login/LoginForm";
-import MisAccionesPage from "./components/MisAccionesPage/MisaccionesPage";
-import SearchForm from "./components/MisAccionesPage/SearchForm";
-import { Navbar } from "./components/Navbar/Navbar";
-import Chart from "./components/Grafico/Chart";
-import Container from "./components/Grafico/Container";
-import MapChart from "./components/Grafico/Map";
-import StockChart from "./components/Grafico/Stock";
-import React, { useState } from "react";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -27,27 +20,18 @@ function App() {
     <div className="App">
       {isLoggedIn ? (
         <BrowserRouter>
-          <Navbar onLogout={handleLogout}>
-            <Routes>
-              <Route path="/" element={<h1>Bienvenido</h1>} />
-              <Route path="/category" element={<ItemListContainer />} />
+          <Navbar onLogout={handleLogout}></Navbar>
+          <Routes>
+            <Route>
+              <Route path="/" element={<ItemListContainer />} />
               <Route
                 path="/category/:categoryName"
                 element={<ItemListContainer />}
               />
               <Route path="/itemDetail/:id" element={<ItemDetailContainer />} />
-              <Route path="/misAcciones" element={<MisAccionesPage />} />
-              <Route path="/mapa" element={<MapChart />} />
               <Route path="*" element={<h1>La ruta no existe</h1>} />
-            </Routes>
-          </Navbar>
-          <SearchForm />
-          <MisAccionesPage />
-          <MapChart />
-          <Container />
-          <Chart />
-          <ItemListContainer />
-          <StockChart />
+            </Route>
+          </Routes>
         </BrowserRouter>
       ) : (
         <LoginForm onLogin={handleLogin} />
